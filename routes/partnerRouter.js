2030738d039fe3ee5839732f2c200ec80a970285
Shahad  Sidek <73951870+shahadsidek@users.noreply.github.com>
2022-12-13 18:23:34 +0300

    Update partnerRouter.js

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -22,7 +22,7 @@ partnerRouter.route('/')
         res.end('Deleting all Partners');
     });
 
-partnerRouter.route('/partners/:partnerId')
+partnerRouter.route('/:partnerId')
     .all((req, res, next) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
